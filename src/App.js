e988c4d979bf97081639e31ecd33514886306d12
faithward <faithward24@gmail.com>
2023-03-10 19:21:04 +0000

    config

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
 import { Navbar } from './navbar.jsx';
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
+    <div class="App">
       <Navbar />
-      <header className="App-header">
+      <header class="App-header">
         <h1>Hello Tooti Froots</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,22 +5,24 @@ export const Navbar = () => {
   //const navigate = useNavigate();
 
   return (
-    <nav>
-      <div>
-        <h1>Tuti Fruti</h1>
-      </div>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/">About Us</a>
-        </li>
-        <li>
-          <a href="/">Shop</a>
-        </li>
-      </ul>
-    </nav>
+    <div class="flex mx-auto justify-center bg-slate-300">
+      <nav class="justify-start">
+        <div>
+          <h1>Tuti Fruti :)</h1>
+        </div>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">About Us</a>
+          </li>
+          <li>
+            <a href="/">Shop</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
